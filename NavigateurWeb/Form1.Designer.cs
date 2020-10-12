8e586a80015bb8513ae7b6253e66456ff1eda4e0
Youssef AMAR <Youssef.AMAR@student.hel.be>
2020-10-12 10:49:53 +0200

    Ajoutez des fichiers projet.

---
+++ NavigateurWeb/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace NavigateurWeb
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRecherche = new System.Windows.Forms.Button();
+            this.btnprecedent = new System.Windows.Forms.Button();
+            this.btnSuivant = new System.Windows.Forms.Button();
+            this.tbRecherche = new System.Windows.Forms.TextBox();
+            this.btnHome = new System.Windows.Forms.Button();
+            this.Explorer = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // btnRecherche
+            // 
+            this.btnRecherche.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnRecherche.Location = new System.Drawing.Point(571, 622);
+            this.btnRecherche.Name = "btnRecherche";
+            this.btnRecherche.Size = new System.Drawing.Size(117, 37);
+            this.btnRecherche.TabIndex = 0;
+            this.btnRecherche.Text = "Recherche";
+            this.btnRecherche.UseVisualStyleBackColor = true;
+            this.btnRecherche.Click += new System.EventHandler(this.btnRecherche_Click);
+            // 
+            // btnprecedent
+            // 
+            this.btnprecedent.Location = new System.Drawing.Point(0, 12);
+            this.btnprecedent.Name = "btnprecedent";
+            this.btnprecedent.Size = new System.Drawing.Size(55, 38);
+            this.btnprecedent.TabIndex = 1;
+            this.btnprecedent.Text = "<";
+            this.btnprecedent.UseVisualStyleBackColor = true;
+            this.btnprecedent.Click += new System.EventHandler(this.btnprecedent_Click);
+            // 
+            // btnSuivant
+            // 
+            this.btnSuivant.Location = new System.Drawing.Point(61, 12);
+            this.btnSuivant.Name = "btnSuivant";
+            this.btnSuivant.Size = new System.Drawing.Size(55, 38);
+            this.btnSuivant.TabIndex = 2;
+            this.btnSuivant.Text = ">";
+            this.btnSuivant.UseVisualStyleBackColor = true;
+            this.btnSuivant.Click += new System.EventHandler(this.btnSuivant_Click);
+            // 
+            // tbRecherche
+            // 
+            this.tbRecherche.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbRecherche.Location = new System.Drawing.Point(282, 581);
+            this.tbRecherche.Name = "tbRecherche";
+            this.tbRecherche.Size = new System.Drawing.Size(750, 26);
+            this.tbRecherche.TabIndex = 3;
+            // 
+            // btnHome
+            // 
+            this.btnHome.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnHome.Location = new System.Drawing.Point(1150, 12);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(74, 38);
+            this.btnHome.TabIndex = 5;
+            this.btnHome.Text = "Home";
+            this.btnHome.UseVisualStyleBackColor = true;
+            this.btnHome.Click += new System.EventHandler(this.btnHome_Click);
+            // 
+            // Explorer
+            // 
+            this.Explorer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Explorer.Location = new System.Drawing.Point(0, 56);
+            this.Explorer.Name = "Explorer";
+            this.Explorer.Size = new System.Drawing.Size(1224, 508);
+            this.Explorer.TabIndex = 6;
+            this.Explorer.Url = new System.Uri("", System.UriKind.Relative);
+            // 
+            // Form
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1224, 671);
+            this.Controls.Add(this.Explorer);
+            this.Controls.Add(this.btnHome);
+            this.Controls.Add(this.tbRecherche);
+            this.Controls.Add(this.btnSuivant);
+            this.Controls.Add(this.btnprecedent);
+            this.Controls.Add(this.btnRecherche);
+            this.Name = "Form";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Youssef Explorer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRecherche;
+        private System.Windows.Forms.Button btnprecedent;
+        private System.Windows.Forms.Button btnSuivant;
+        private System.Windows.Forms.TextBox tbRecherche;
+        private System.Windows.Forms.Button btnHome;
+        private System.Windows.Forms.WebBrowser Explorer;
+    }
+}
+
---
+++ NavigateurWeb/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace NavigateurWeb
+{
+    public partial class Form : System.Windows.Forms.Form
+    {
+
+        string hel = "https://www.hel.be/";
+
+        public Form()
+        {          
+            InitializeComponent();
+            Explorer.ScriptErrorsSuppressed = true;
+            Explorer.Url = new Uri(hel);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnprecedent_Click(object sender, EventArgs e)
+        {
+            if(Explorer.CanGoBack)
+
+            Explorer.GoBack();
+        }
+
+        private void btnSuivant_Click(object sender, EventArgs e)
+        {
+            if(Explorer.CanGoForward)
+
+            Explorer.GoForward();
+        }
+
+        private void btnRecherche_Click(object sender, EventArgs e)
+        {
+            string page = tbRecherche.Text.Trim();
+
+            if (!page.StartsWith("http://") && !page.StartsWith("https://"))
+            {
+                page = "http://" + page;
+            }
+
+            Explorer.Navigate(page);
+        }
+
+        private void btnHome_Click(object sender, EventArgs e)
+        {
+            Explorer.Navigate(hel);
+        }
+    }
+}
